6d501413b75611a631bde310365f82d3323c0db9
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-19 22:31:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,16 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  #def initialize(options = {})
+  #  super(options)
+  #end
 
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
+    DB[:conn].execute(sql, name)
+  end
 end
