f7a682368e013dca702c5a4d2c5dc899597f7829
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-19 23:31:57 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,5 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'pry'
 
 class InteractiveRecord
   def initialize(options = {})
@@ -46,7 +45,6 @@ class InteractiveRecord
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
-    binding.pry
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
