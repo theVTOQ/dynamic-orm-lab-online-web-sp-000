0e575ed5fb5bfe33b5c39c978f03010b44de15d6
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-19 23:32:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student < InteractiveRecord
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
   end
 end
