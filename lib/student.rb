5b84cdc434598988424de339c62e3ad251014dab
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-19 22:52:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student < InteractiveRecord
   #end
 
   self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym unless col_name.is_a? Integer
+    attr_accessor col_name.to_sym
   end
 
   def self.find_by_name(name)
